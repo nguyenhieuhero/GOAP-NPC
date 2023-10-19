4c26618a3d2c6a66cbb4c5249326e521bbefaef7
fliMMM <bachkame@gmail.com>
2023-10-19 07:38:01 +0700

    feature

--- Player/player.gd
+++ Player/player.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 class_name Actor
 
-@export var SPEED: float = 100
+@export var SPEED: float = 50
 @export var direction: Vector2 = Vector2(0, 1)
 @onready var animation_tree = $AnimationTree
 @onready var inventory = $Inventory
 @onready var isAction = false
+@onready var nav:NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
 	animation_tree.active=true
 func _physics_process(_delta):
-	if not isAction:
-		velocity = direction * SPEED
-	else:
+	if isAction:
 		velocity = Vector2.ZERO
 	move_and_slide()
 func _process(delta):
-	direction = Input.get_vector("left","right","up","down")
-	if isAction:
-		return
-	if direction != Vector2.ZERO:
-		update_animation_direction(direction)	
-	update_state()
+	pass
+#	direction = Input.get_vector("left","right","up","down")
+#	if isAction:
+#		return
+#	if direction != Vector2.ZERO:
+#		update_animation_direction(direction)	
+#
+#	update_state()
+
+	
+	
+	
 	
 	
 func update_animation_direction(input: Vector2):
@@ -46,8 +51,12 @@ func action_finished():
 func set_idle():
 	animation_tree.set("parameters/conditions/is_walk", false)
 	animation_tree.set("parameters/conditions/is_idle", true)
+	
 
 
-func _input(event):
-	if(event.is_action_pressed("toggle_inventory")):
-		inventory.toggle()
+	
+	
+
+
+
+
